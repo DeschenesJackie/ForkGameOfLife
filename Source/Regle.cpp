f787dd88d3b663ba48e45d8a355c484eb921b0cc
Deschênes Jackie <1704936@decinfo.cvm>
2020-03-09 12:11:20 -0400

    creation vecteur regle fonction de confirmation incomplete

--- Source/Regle.cpp
+++ Source/Regle.cpp
@@ -1,4 +1,4 @@
-#include "Regle.h"
+#include "..\Header\Regle.h"
 
 Regle::Regle(std::string regleEntree)
 : Regle(Regle::genRegleBorn(regleEntree), Regle::genRegleSurvive(regleEntree))
@@ -13,11 +13,30 @@ Regle::Regle(std::vector<int> regleBorn, std::vector<int> regleSurvive)
 //Rendu ici, définir les regles genRegleBorn et genRegleSurvive
 std::vector<int> Regle::genRegleBorn(std::string mRegleEntree)
 {
+	std::vector<int> vecteur;
+	std::string::iterator iterateurString = mRegleEntree.begin();
+	
+	while (*iterateurString != '/') {
+		if (*iterateurString != 'B' || *iterateurString != 'b') vecteur.push_back((*iterateurString - '0'));
+		++iterateurString;
+	}
+
 	return std::vector<int>();
 }
 
 std::vector<int> Regle::genRegleSurvive(std::string mRegleEntree)
 {
+	std::vector<int> vecteur;
+	std::string::iterator iterateurString = mRegleEntree.begin();
+
+	while (*iterateurString != '/') {
+		++iterateurString;
+	}
+	++iterateurString;
+	while (iterateurString != mRegleEntree.end()) {
+		if (*iterateurString != 'S' || *iterateurString != 's') vecteur.push_back((*iterateurString - '0'));
+	}
+
 	return std::vector<int>();
 }
 
