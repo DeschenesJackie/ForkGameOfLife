1f41fd942549899fa3a44bee74dc92c6b5440692
Deschênes Jackie <1704936@decinfo.cvm>
2020-03-09 13:38:05 -0400

    not commentaire

--- Source/Controleur.cpp
+++ Source/Controleur.cpp
@@ -9,7 +9,7 @@ Controleur::Controleur(Univers & univers, Vue & vue)
 
 void Controleur::start() {
 	while (true) { // A changer pour condition d'arret
-		//mUnivers.evolve();
+		mUnivers.evolve();
 		// Fonction d'affichage du VecteurActif de la vue 
 		// appelle pour avoir access au vecteur Actif est mUnivers.getVecteurActif()
 	}
